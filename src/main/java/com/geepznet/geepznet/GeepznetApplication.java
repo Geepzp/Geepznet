4e614ee691ba4f81b20c9b443702d2326d543e13
Godspower <geepzp@gmail.com>
2022-11-02 19:24:43 +0100

    first commit

---
+++ src/main/java/com/geepznet/geepznet/GeepznetApplication.java
@@ -0,0 +1,13 @@
+package com.geepznet.geepznet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeepznetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeepznetApplication.class, args);
+	}
+
+}
